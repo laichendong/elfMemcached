02ab53c7ef41df8a4a8feffcc32290b1c8a60d82
lai chendong <laichendong@gmail.com>
2011-12-03 14:19:39 +0800

    hello world for commit

---
+++ src/com/elf/memcached/MemcachedClient.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.elf.memcached;
+
+/**
+ * @author laichendong
+ *
+ */
+public class MemcachedClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
