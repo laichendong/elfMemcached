3b79ed6ea11d693376937c66c75774c7a5990e53
lai chendong <laichendong@gmail.com>
2012-01-12 18:03:59 +0800

    ...

--- src/com/elf/memcached/MemcachedClient.java
+++ src/com/elf/memcached/MemcachedClient.java
@@ -33,17 +33,47 @@ public interface MemcachedClient {
 	public abstract boolean set(String key, Object value, long exptime);
 	
 	/**
+	 * 仅当服务器<b>没有</b>用这个key存储数据时，向服务器存储这个k,v对。和{@link com.elf.memcached.MemcachedClient#replace(String, Object)}
+	 * 刚好相反。（不过期，设置过期时间请用： {@link com.elf.memcached.MemcachedClient#add(String, Object, long)})
 	 * 
-	 * @param key
-	 * @param value
-	 * @return
+	 * @param key 存储使用的key
+	 * @param value 存储的value
+	 * @return 如果存储成功，则返回true，否则返回false。
 	 */
 	public abstract boolean add(String key, Object value);
 	
+	/**
+	 * 仅当服务器<b>没有</b>用这个key存储数据时，向服务器存储这个k,v对。和{@link com.elf.memcached.MemcachedClient#replace(String, Object, long)}
+	 * 刚好相反。用exptime设置过期时间。
+	 * 如果设置的小于60*60*24*30（30天），表示从设置到exptime秒后过期；如果大于等于这个值，则表示exptime是一个unix时间戳。到这个时间戳表示的时间过期
+	 * 
+	 * @param key 存储使用的key
+	 * @param value 存储的value
+	 * @param exptime 过期时间，单位为秒
+	 * @return 如果存储成功，则返回true，否则返回false。
+	 */
 	public abstract boolean add(String key, Object value, long exptime);
 	
+	/**
+	 * 仅当服务器<b>有</b>用这个key存储数据时，向服务器存储这个k,v对。和{@link com.elf.memcached.MemcachedClient#add(String, Object)}
+	 * 刚好相反。（不过期，设置过期时间请用： {@link com.elf.memcached.MemcachedClient#replace(String, Object, long)})
+	 * 
+	 * @param key 存储使用的key
+	 * @param value 存储的value
+	 * @return 如果存储成功，则返回true，否则返回false。
+	 */
 	public abstract boolean replace(String key, Object value);
 	
+	/**
+	 * 仅当服务器<b>有</b>用这个key存储数据时，向服务器存储这个k,v对。和{@link com.elf.memcached.MemcachedClient#add(String, Object, long)}
+	 * 刚好相反。用exptime设置过期时间。
+	 * 如果设置的小于60*60*24*30（30天），表示从设置到exptime秒后过期；如果大于等于这个值，则表示exptime是一个unix时间戳。到这个时间戳表示的时间过期
+	 * 
+	 * @param key 存储使用的key
+	 * @param value 存储的value
+	 * @param exptime 过期时间，单位为秒
+	 * @return 如果存储成功，则返回true，否则返回false。
+	 */
 	public abstract boolean replace(String key, Object value, long exptime);
 	
 	public abstract boolean append(String key, Object value);
--- test/com/elf/memcached/ASCIIClientTest.java
+++ test/com/elf/memcached/ASCIIClientTest.java
@@ -5,7 +5,9 @@ package com.elf.memcached;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import junit.framework.Assert;
 
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -15,47 +17,90 @@ import org.junit.Test;
  */
 public class ASCIIClientTest {
 	
-	MemcachedClient asciiClient;
+	static MemcachedClient asciiClient;
 	
+	/**
+	 * 初始化一个有用的客户端
+	 */
 	@BeforeClass
-	public void init() {
+	public static void init() {
 		MemcachedConnectionPool pool = new MemcachedConnectionPool(new String[] { "10.90.100.220:11211",
 				"10.90.100.220:11211", "10.90.100.220:11211" });
 		pool.initialize();
 		asciiClient = new ASCIIClient(pool);
 	}
 	
-	
-	/**
-	 * Test method for {@link com.elf.memcached.ASCIIClient#set(java.lang.String, java.lang.Object)}.
-	 */
-	@Test
-	public void testSetStringObject() {
-		assertTrue(asciiClient.set("k", "v"));
+	@After
+	public void clean() {
+		asciiClient.delete("k");
 	}
 	
 	/**
-	 * Test method for {@link com.elf.memcached.ASCIIClient#set(java.lang.String, java.lang.Object, long)}.
+	 * 测试{@link com.elf.memcached.ASCIIClient#set(java.lang.String, java.lang.Object)}.
+	 * 能正确返回true 且 用get能拿到预期的值。
 	 */
 	@Test
-	public void testSetStringObjectLong() {
-		assertTrue(asciiClient.set("k", "v", 100L));
+	public void test_set() {
+		Assert.assertTrue(asciiClient.set("k", "v"));
+		Assert.assertEquals("v", asciiClient.get("k"));
 	}
 	
 	/**
-	 * Test method for {@link com.elf.memcached.ASCIIClient#add(java.lang.String, java.lang.Object)}.
+	 * 测试{@link com.elf.memcached.ASCIIClient#set(java.lang.String, java.lang.Object, long)}.
+	 * 能正确返回true 且在有效期内可以 用get能拿到预期的值。
 	 */
 	@Test
-	public void testAddStringObject() {
-		fail("Not yet implemented");
+	public void test_set_with_exptime() {
+		Assert.assertTrue(asciiClient.set("k", "v", 10L));
+		Assert.assertEquals("v", asciiClient.get("k"));
 	}
 	
 	/**
-	 * Test method for {@link com.elf.memcached.ASCIIClient#add(java.lang.String, java.lang.Object, long)}.
+	 * 测试{@link com.elf.memcached.ASCIIClient#set(java.lang.String, java.lang.Object, long)}.
+	 * 能正确返回true 且在有效期过后不再可以 用get能拿到值。
+	 * 
+	 * @throws InterruptedException
 	 */
 	@Test
-	public void testAddStringObjectLong() {
-		fail("Not yet implemented");
+	public void test_set_exptime() throws InterruptedException {
+		Assert.assertTrue(asciiClient.set("k", "v", 1L));
+		Thread.sleep(1000);
+		System.out.println(asciiClient.get("k"));
+		Assert.assertNull(asciiClient.get("k"));
+	}
+	
+	/**
+	 * 测试{@link com.elf.memcached.ASCIIClient#add(java.lang.String, java.lang.Object)}.
+	 * 当服务器里没有这个key时，存储成功
+	 */
+	@Test
+	public void test_add() {
+		Assert.assertTrue(asciiClient.add("k", "v"));
+		Assert.assertEquals(asciiClient.get("k"), "v");
+	}
+	
+	/**
+	 * 测试{@link com.elf.memcached.ASCIIClient#add(java.lang.String, java.lang.Object)}.
+	 * 当服务器里有这个key时，存储失败
+	 */
+	@Test
+	public void test_add_exist_key() {
+		Assert.assertTrue(asciiClient.add("k", "v"));
+		Assert.assertTrue(!asciiClient.add("k", "v1"));
+		Assert.assertEquals(asciiClient.get("k"), "v");
+	}
+	
+	/**
+	 * 测试{@link com.elf.memcached.ASCIIClient#add(java.lang.String, java.lang.Object, long)}.
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void test_add_with_exptime() throws InterruptedException {
+		Assert.assertTrue(asciiClient.add("k", "v", 1L));
+		Assert.assertEquals("v", asciiClient.get("k"));
+		Thread.sleep(1000);
+		Assert.assertNull(asciiClient.get("k"));
 	}
 	
 	/**
@@ -79,7 +124,7 @@ public class ASCIIClientTest {
 	 */
 	@Test
 	public void testAppendStringObject() {
-		fail("Not yet implemented");
+		Assert.assertTrue(!asciiClient.append("testAppendStringObject", "testAppendStringObject"));
 	}
 	
 	/**
